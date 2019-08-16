929c3afd503ca897460fe2760c3dddc72f551322
Egon Willighagen <egon.willighagen@gmail.com>
2019-08-16 15:28:08 +0200

    Working implementation + test suite

--- src/input.js
+++ src/input.js
@@ -1,26 +1,28 @@
-import { utils } from '@citation-js/core'
+import { util } from '@citation-js/core'
 
 export const ref = '@pubmed'
 export const formats = {
   // fetch from API
   '@pubmed/id': {
     parseAsync (id) {
+      id = id.replace("pmid:","")
       const url = `https://api.ncbi.nlm.nih.gov/lit/ctxp/v1/pubmed/?format=csl&id=${id}`
       const headers = {}
 
-      return utils.fetchFileAsync(url, { headers })
+      return util.fetchFileAsync(url, { headers })
     },
     parseType: {
       dataType: 'String',
       predicate: /^pmid:\d+$/
     }
   },
-  '@pubmed/pmcid': {{
+  '@pubmed/pmcid': {
     parseAsync (id) {
+      id = id.replace("PMC","")
       const url = `https://api.ncbi.nlm.nih.gov/lit/ctxp/v1/pmc/?format=csl&id=${id}`
       const headers = {}
 
-      return utils.fetchFileAsync(url, { headers })
+      return util.fetchFileAsync(url, { headers })
     },
     parseType: {
       dataType: 'String',
@@ -28,5 +30,3 @@ export const formats = {
     }
   }
 }
-
-export { ref, formats }
